f0a3d24823b232e0a3509bf20f894f0efd56ece2
Aksh <jaiswal.aksh2005@gmail.com>
2025-04-17 10:52:03 +0530

    area

--- day2/area.java
+++ day2/area.java
@@ -1,3 +1,18 @@
-class area calculator{
-    double area(double length, )
+class areaCalculator{
+    double area(double length, double breadth ){
+    return length*breadth;
+}
+    double area(double radius){
+        return 3.14*radius*radius;
+    }
+}
+public class area{
+    public static void main(String[] args) {
+        areaCalculator calc =new areaCalculator();
+        double rectangleArea = calc.area(8.0,5.0);
+        double circleArea = calc.area(7);
+
+        System.out.println("area of rectangle: " + rectangleArea);
+        System.out.println("area of circle: " + circleArea);
+    }
 }

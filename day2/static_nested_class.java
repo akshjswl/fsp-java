afd004921624d54409817dddea9e123396d4a054
Aksh <jaiswal.aksh2005@gmail.com>
2025-04-17 12:22:26 +0530

    static nested class

--- day2/static_nested_class.java
+++ day2/static_nested_class.java
@@ -1,6 +1,6 @@
 // wap to display use of static nested class
 
-class outer {
+class Outer {
     static int a=10;
     int c=30;
     void disp1()
@@ -15,7 +15,14 @@ class outer {
             System.out.println(b);
             System.out.println(a);
             Outer ob2=new Outer();
-            system
+            System.out.println(ob2.c);
         }
+    }
+}
+
+class static_nested_class{
+    public static void main(String[] args) {
+        Outer ob=new Outer();
+        ob.disp1();
     }
 }

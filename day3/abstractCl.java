95b6699ab872030dde2c17b6e369e4d9419d35b3
Aksh <jaiswal.aksh2005@gmail.com>
2025-04-18 11:08:41 +0530

    n

--- day3/abstractCl.java
+++ day3/abstractCl.java
@@ -7,4 +7,21 @@ abstract class parent{
         System.out.println("testing for abstract class");
     }
 }
-class Child extends 
+class Child extends parent{
+    void display1()
+    {
+        System.out.println("this body is provided by child class");
+    }
+    void display3()
+    {
+        System.out.println("Child");
+    }
+}
+class abstractCl{
+    public static void main(String[] args) {
+        Child t=new Child();
+        t.display1();
+        t.display2();
+        t.display3();
+    }
+}
